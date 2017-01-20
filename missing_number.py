75852b8015a23c4ed4625616a30bd7a7575d4190
Cynthia Wasonga <wasonga.cynthia@yahoo.in>
2017-01-20 10:08:56 +0300

    Made code more efficient with set symmetry

--- missing_number.py
+++ missing_number.py
@@ -1,9 +1,5 @@
 def find_missing(l1, l2):
-	extra = set(l1)-set(l2)
+	extra = set(l1)^set(l2)
 	if extra:
 		return extra.pop()
-	else:
-		extra = set(l2)-set(l1)
-		if extra:
-			return extra.pop()
 	return 0
